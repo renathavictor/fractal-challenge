3ccdcbeacb0acd0a50456c13039a717e42dd1ac9
Renatha <renatha.nvictor@gmail.com>
2019-12-12 19:24:07 -0300

    adicionado o webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello word!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root')) */
+console.log('ok')
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        /* use: ['babel-loader'] */
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  }
+}
